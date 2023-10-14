357cb4d03d952137c6d3f8d5dd9f5a98e7f15466
Siddharth Sircar <111794453+spongeMouse93@users.noreply.github.com>
2023-10-14 10:44:15 -0400

    Create RunProject1.java

---
+++ src/csevent/RunProject1.java
@@ -0,0 +1,17 @@
+package csevent;
+
+/**
+ * This is the main class for running the entire project.
+ * @author Siddharth Sircar
+ * @author Yash Shah
+ * @since September 18, 2023
+ */
+public class RunProject1 {
+    /**
+     * The main method for running the entire project
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args){
+        new EventOrganizer().run();
+    }
+}
