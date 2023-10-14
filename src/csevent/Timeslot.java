a9a7f214ba3eae009f78cfb2811fe7ff56d58d5e
Siddharth Sircar <111794453+spongeMouse93@users.noreply.github.com>
2023-10-14 10:39:00 -0400

    Create Timeslot.java

---
+++ src/csevent/Timeslot.java
@@ -0,0 +1,23 @@
+package csevent;
+
+/**
+ * A helper enum made to handle the 3 timeslots for scheduling events: morning, a
+ * afternoon, and evening.
+ * @author Siddharth Sircar
+ * @author Yash Shah
+ * @since September 18, 2023
+ */
+public enum Timeslot {
+    /**
+     * Handles the morning timeslot, 10:30 am
+     */
+    MORNING,
+    /**
+     * Handles the afternoon timeslot, 2:00 pm
+     */
+    AFTERNOON,
+    /**
+     * Handles the evening timeslot: 6:30 pm
+     */
+    EVENING;
+}
