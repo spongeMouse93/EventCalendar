5f19430e3a30329512dafbdae592551303cb1b5a
Siddharth Sircar <111794453+spongeMouse93@users.noreply.github.com>
2023-10-14 10:38:11 -0400

    Create Department.java

---
+++ src/csevent/Department.java
@@ -0,0 +1,30 @@
+package csevent;
+
+/**
+ * A helper enum made to handle the main departments here at Rutgers
+ * @author Siddharth Sircar
+ * @author Yash Shah
+ * @since September 18, 2023
+ */
+public enum Department {
+    /**
+     * Handles the Computer Science department
+     */
+    CS,
+    /**
+     * Handles the Electrical Engineering department
+     */
+    EE,
+    /**
+     * Handles the Mathematics department
+     */
+    MATH,
+    /**
+     * Handles the Information Technology and Informatics department
+     */
+    ITI,
+    /**
+     * Handles the Business Analytics and Information Technology department
+     */
+    BAIT
+}
